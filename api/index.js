799a5809cb5a5dfef5742cbb7506e5c48bc6bbfc
Muhammad-Aslambik <job.muhammadaslambik@gmail.com>
2026-09-19 22:39:52 +0700

    Express App langsung, tanpa perlu serverless-http sama sekal

--- api/index.js
+++ api/index.js
@@ -1,16 +1,8 @@
 console.log("=== api/index.js MULAI DIEKSEKUSI ===");
 
-const serverless = require("serverless-http");
-console.log("=== serverless-http berhasil di-require ===");
-
 const app = require("../server");
-console.log("=== ../server (Express app) berhasil di-require ===");
 
-const handler = serverless(app);
-console.log("=== handler serverless berhasil dibuat ===");
-
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   console.log("=== handler DIPANGGIL untuk:", req.method, req.url, "===");
-  await handler(req, res);
-  console.log("=== handler SELESAI untuk:", req.method, req.url, "===");
+  app(req, res);
 };
